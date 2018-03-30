b5a2cc9f71bdef07502f1dd6e5355ff710b8a41d
Torgeir Helgevold <tor@nrwl.io>
2018-03-29 23:09:07 -0400

    Added Basic Java api sample

--- src/hello-world/hello-world.component.ts
+++ src/hello-world/hello-world.component.ts
@@ -1,14 +1,30 @@
-import {Component, NgModule} from '@angular/core';
-import {msg} from '../lib/file';
+import { Component, NgModule, OnInit } from "@angular/core";
+import { msg } from "../lib/file";
+import { Jsonp } from "@angular/http";
+import "rxjs/add/operator/map";
 
 @Component({
-  selector: 'hello-world-app',
+  selector: "hello-world-app",
   template: `
     <div>Hello {{ name }}</div>
     <input type="text" [value]="name" (input)="name = $event.target.value"/>
+    <div *ngFor="let car of cars">{{car}}</div>
   `,
-  styleUrls: ['./hello-world-styles.css']
+  styleUrls: ["./hello-world-styles.css"]
 })
-export class HelloWorldComponent {
+export class HelloWorldComponent implements OnInit {
   name: string = msg;
+
+  cars = [];
+
+  constructor(private http: Jsonp) {}
+
+  ngOnInit() {
+    this.http
+      .request("http://localhost:8080/cars?callback=JSONP_CALLBACK")
+      .map(res => res.json())
+      .subscribe(res => {
+        this.cars = res;
+      });
+  }
 }
--- src/hello-world/hello-world.module.ts
+++ src/hello-world/hello-world.module.ts
@@ -1,9 +1,12 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import {HelloWorldComponent} from './hello-world.component';
+import { HelloWorldComponent } from "./hello-world.component";
+import { JsonpModule } from "@angular/http";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [HelloWorldComponent],
   exports: [HelloWorldComponent],
+  imports: [JsonpModule, CommonModule]
 })
 export class HelloWorldModule {}
